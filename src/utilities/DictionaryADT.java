28510e1b8c442bc0d7b60b8345dfe2317222d025
DinhAnh2301 <dinhanh2301@gmail.com>
2024-10-24 20:57:28 -0600

    DictionaryADT.java completed

--- src/utilities/DictionaryADT.java
+++ src/utilities/DictionaryADT.java
@@ -13,15 +13,38 @@ package utilities;
 public interface DictionaryADT<K,V>
 {
 	/**
+	 * Insert a key - value pair into the dictionary where every key is unique.
+	 * If the inserted key already exists, an exception will be thrown.
 	 * 
-	 * @param key
-	 * @param value
+	 * @param key the inserted key
+	 * @param value the inserted value that is paired with the key
+	 * @exception NullArgument thrown when the key or the value is null
+	 * @exception DuplicatedKey thrown when the inserted key already exists
 	 */
 	void insert(K key, V value);
 	
+	/**
+	 * Remove a pair in the dictionary based on the key
+	 * 
+	 * @param key the key to be removed
+	 * @exception NullArgument thrown when the key or the value is null
+	 */
 	V remove(K key);
 	
+	/**
+	 * Update the value of an existing key
+	 * 
+	 * @param key the existing key
+	 * @param value the value to be updated
+	 * @exception NullArgument thrown when the key or the value is null
+	 */
 	V update(K key, V value);
 	
+	/**
+	 * Look up a value based on the key
+	 * 
+	 * @param key the key to be found
+	 * @exception NullArgument thrown when the key or the value is null
+	 */
 	V find(K key);
 }
