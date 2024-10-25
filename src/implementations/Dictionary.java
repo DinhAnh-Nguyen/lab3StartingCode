64e279f2450a0b1954ffc8e68dc12fc6f8c05d9b
DinhAnh2301 <dinhanh2301@gmail.com>
2024-10-25 01:16:05 -0600

    insert and remove method added

--- src/implementations/Dictionary.java
+++ src/implementations/Dictionary.java
@@ -6,6 +6,8 @@ import exceptions.NullKeyException;
 import exceptions.NullValueException;
 import exceptions.DuplicatedKeyException;
 
+// Author: Dinh Anh Nguyen, Mark Bui
+
 public class Dictionary<K,V> implements DictionaryADT<K,V>
 {
 	// constant
@@ -35,23 +37,49 @@ public class Dictionary<K,V> implements DictionaryADT<K,V>
 		this.values = values;
 	}
 	
-	
+	// Method for inserting a new key - value pair with an unique key
 	@Override
-	public void insert(K key, V value) throws NullKeyException {
-		if (key == null || value == null) {
+	public void insert(K key, V value) throws NullKeyException, NullValueException, DuplicatedKeyException {
+		if (key == null) {
 			throw new NullKeyException();
 		}
+		
+		if (value == null) {
+			throw new NullValueException();
+		}
+		
+		if (keys.contains(key)) {
+			throw new DuplicatedKeyException("Error! " + key + " already existed.");
+		}
+		
+		keys.add(key);
+		values.add(value);
 	}
+	
+	// Method for removing a key - value pair based on the key
 	@Override
-	public V remove(K key) {
-		// TODO Auto-generated method stub
-		return null;
+	public V remove(K key) throws NullKeyException {
+		if (key == null) {
+			throw new NullKeyException();
+		}
+		
+		int i = keys.indexOf(key);
+		if (i == -1) {
+			System.out.println("Key not found. Try again.");
+		}
+		
+		V removedValue = values.get(i);
+		keys.remove(i);
+		values.remove(i);
+		return removedValue;
 	}
+	
 	@Override
 	public V update(K key, V value) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
 	@Override
 	public V find(K key) {
 		// TODO Auto-generated method stub
