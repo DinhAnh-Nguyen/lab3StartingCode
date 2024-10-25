9d324324871255c038e75e8b114ad2c3b6384483
DinhAnh2301 <dinhanh2301@gmail.com>
2024-10-24 23:16:37 -0600

    2 exception classes added

--- src/exceptions/NullArgumentException.java
+++ src/exceptions/NullArgumentException.java
@@ -6,5 +6,9 @@ public class NullArgumentException extends Exception {
 	 * 
 	 */
 	private static final long serialVersionUID = -6331941262757255734L;
+	
+	public NullArgumentException(String errorMessage) {
+		super(errorMessage);
+	}
 
 }
